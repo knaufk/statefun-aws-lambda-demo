2618250f0620d8c83969f7a5484482bcbeb3222b
Konstantin Knauf <knauf.konstantin@gmail.com>
2021-02-17 18:43:09 +0100

    AWS Lambda with multiple path template

--- gateway.tf
+++ gateway.tf
@@ -2,29 +2,29 @@
 resource "aws_apigatewayv2_api" "api" {
   name          = "StatefulFunctions"
   protocol_type = "HTTP"
-  target = aws_lambda_function.greeter.arn
 }
 
-//resource "aws_apigatewayv2_integration" "greeter" {
-//  api_id           = aws_apigatewayv2_api.api.id
-//  integration_type = "AWS_PROXY"
-//
-//  integration_method        = "POST"
-//  integration_uri           = aws_lambda_function.greeter.invoke_arn
-//}
-//
-//resource "aws_apigatewayv2_route" "greeter" {
-//  api_id    = aws_apigatewayv2_api.api.id
-//  route_key = "ANY /greeter/{proxy+}"
-//
-//  target = "integrations/${aws_apigatewayv2_integration.greeter.id}"
-//}
-//
-//resource "aws_apigatewayv2_stage" "default" {
-//  api_id = aws_apigatewayv2_api.api.id
-//  name   = "default"
-//  auto_deploy = true
-//}
+resource "aws_apigatewayv2_integration" "greeter" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.greeter.invoke_arn
+  payload_format_version    = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "greeter" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /greeter"
+
+  target = "integrations/${aws_apigatewayv2_integration.greeter.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id = aws_apigatewayv2_api.api.id
+  name   = "$default"
+  auto_deploy = true
+}
 
 output "base_url" {
   value = aws_apigatewayv2_api.api.api_endpoint
